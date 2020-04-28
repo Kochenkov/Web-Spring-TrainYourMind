c22a923c4f0e453e499adb3a5fe1109b8c5903a8
Vlad Kochenkov <vkochenkov@alfabank.ru>
2020-04-28 18:08:47 +0300

    добавил lombok

--- src/main/java/ru/kochenkov/tym/models/User.java
+++ src/main/java/ru/kochenkov/tym/models/User.java
@@ -1,5 +1,6 @@
 package ru.kochenkov.tym.models;
 
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -17,6 +18,7 @@ import javax.persistence.Table;
 import java.util.Collection;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "usr")
 public class User implements UserDetails {
@@ -44,46 +46,6 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
@@ -101,16 +63,11 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return isActive();
+        return active;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return getRoles();
-    }
-
-    @Override
-    public String toString() {
-        return this.username;
     }
 }
